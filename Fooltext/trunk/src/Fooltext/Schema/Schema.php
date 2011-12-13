1dccc427f81686d84c2436bcdafa11019a89f424
daniel.menard.35@gmail.com <daniel.menard.35@gmail.com@444a106b-eed5-e7b0-2fd9-c3bf2e9ceeb0>
2011-12-13 18:56:40 +0000

    L'entête du fichier était erronné (en tout cas, différent de celui des autres fichiers).

--- Fooltext/trunk/src/Fooltext/Schema/Schema.php
+++ Fooltext/trunk/src/Fooltext/Schema/Schema.php
@@ -2,18 +2,8 @@
 /**
  * This file is part of the Fooltext package.
  *
- * Copyright (C) 1997-2011 Daniel Ménard
- *
- * This program is free software: you can redistribute it and/or modify it under the terms of
- * the GNU General Public License as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program.
- * If not, see <http://www.gnu.org/licenses/gpl.html>.
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
  *
  * @package     Fooltext
  * @subpackage  Schema
