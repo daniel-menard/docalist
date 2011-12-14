e635405fe0f6301bebf43f552e2ccac54e36153a
daniel.menard.35@gmail.com <daniel.menard.35@gmail.com@444a106b-eed5-e7b0-2fd9-c3bf2e9ceeb0>
2011-12-14 10:38:01 +0000

    - Nouvelle interface DocumentInterface - Modification de la classe Document pour qu'elle implémente cette interface.

--- Fooltext/trunk/src/Fooltext/Document/Document.php
+++ Fooltext/trunk/src/Fooltext/Document/Document.php
@@ -1,20 +1,76 @@
 <?php
+/**
+ * This file is part of the Fooltext package.
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Fooltext
+ * @subpackage  Document
+ * @author      Daniel Ménard <Daniel.Menard@laposte.net>
+ * @version     SVN: $Id: AnalyzerInterface.php 10 2011-12-13 15:45:47Z daniel.menard.35@gmail.com $
+ */
 namespace Fooltext\Document;
-use Fooltext\Schema\Schema;
 
-// Un enregistrement de la base
-class Document// extends \Multimap
+/**
+ * Représente un document de la base.
+ *
+ */
+class Document implements DocumentInterface
 {
+    /**
+     * Les données du document.
+     *
+     * @var array
+     */
+    protected $data;
 
     public function __construct(array $data = array())
     {
         $this->data = $data;
-        return;
-        $this->compareMode(self::CMP_EQUAL);
-        foreach($data as $key=>$value)
-        {
-            $this->add($key, $value);
-        }
+    }
+
+    public function __get($field)
+    {
+        return isset($this->data[$field]) ? $this->data[$field] : null;
+    }
+
+    public function __set($field, $value)
+    {
+        $this->data[$field] = $value;
+    }
+
+    public function __isset($field)
+    {
+        return isset($this->data[$field]);
+    }
+
+    public function __unset($field)
+    {
+        unset($this->data[$field]);
+    }
+
+    /**
+     * Retourne un itérateur sur la liste des champs présents
+     * dans le document.
+     *
+     * Permet d'utiliser un objet Document dans une boucle foreach.
+     *
+     * @return \ArrayAccess
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->data);
+    }
+
+    /**
+     * Retourne le nombre de champs présents dans le document.
+     *
+     * @return int
+     */
+    public function count ()
+    {
+        return count($this->data);
     }
 
     public function toArray()
