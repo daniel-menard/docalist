cee5fd2506b10aee88de5c64eabbe4063adb367d
Daniel Ménard <daniel-menard@users.noreply.github.com>
2011-12-21 10:09:24 +0000

    Commit en l'état du script de test que j'utilise.

--- Fooltext/trunk/dev.php
+++ Fooltext/trunk/dev.php
@@ -1,22 +1,35 @@
 <?php
 header('content-type: text/html; charset=utf-8');
-
-echo '<pre>';
 require_once(__DIR__ . '/autoload.php');
-
+new XapianQuery();
 // test query
-use Fooltext\Query\Query;
-use Fooltext\Query\QueryOr;
-use Fooltext\Query\QueryAnd;
+use Fooltext\Query\OrQuery;
+use Fooltext\Query\AndQuery;
+use Fooltext\Query\NotQuery;
+use Fooltext\Query\AndMaybeQuery;
+use Fooltext\Query\PhraseQuery;
+use Fooltext\Query\NearQuery;
+use Fooltext\Query\WildcardQuery;
+
 if (false)
 {
-    $q1 = new QueryOr('a','b');
-    $q2 = new QueryOr('c','d');
-    $q = new QueryOr($q1, $q2);
+    $q1 = new OrQuery('a','b');
+
+    var_export($q1->getType());
+    die();
+    $q2 = new OrQuery('c','d');
+    $q = new OrQuery($q1, $q2);
+    $q = new AndQuery($q, 'z');
     echo "q1 : $q1<br />q2: $q2<br />result: $q<br /><br />";
-    $q1->dump('q1');
-    $q2->dump('q1');
-    $q->dump('result');
+//     $q1->dump('q1');
+//     $q2->dump('q1');
+//     $q->dump('result');
+
+    echo "Après optimisation : ", $q->optimize(), '<br />';
+
+    $q = new AndQuery('a', array('b','c',new AndQuery('d','e')));
+    echo "Avant optimisation : ", $q, '<br />';
+    echo "Après optimisation : ", $q->optimize(), '<br />';
 
     die();
 }
@@ -24,18 +37,42 @@ if (false)
 use Fooltext\QueryParser\Lexer;
 use Fooltext\QueryParser\Parser;
 
-$equation = "typdoc=article date:2011 titre:\"l'hôpital dans tous ses états\" motscles=[humour]";
-$equation = 'a OR b +(c NOT z) d OR e AND D near E';
-$equation = 'a "b c" "d e"';
+//$equation = "typdoc=article date:2011 titre:\"l'hôpital dans tous ses états\" motscles=[humour]";
+$equation = 'a OR b +c d OR e AND D near E';
+//$equation = 'a "b c" "d e"';
 //$equation = 'a b titre:+';
+$equation = 'a OR b AND c NOT d';
+$equation = '+';
 
+echo "<p>Equation analysée :<br /><code>$equation</code></p>";
+
+echo "<p>Tokens reconnus par l'analyseur lexical :<br />";
 $lexer = new Lexer();
 $lexer->dumpTokens($equation);
+echo "</p>";
 
 $parser = new Parser();
 $query = $parser->parseQuery($equation);
-echo "<hr />", $query, "<hr />";
-var_export($query);
+echo "<p>Equation générée par le query parser :<br /><code>", $query, "</code></p>";
+echo "<p>Equation optimisée :<br /><code>", $query->optimize(), "</code></p>";
+
+
+// $q = $query->toXapian();
+// echo $q->get_description(), '<br />';
+
+$qp=new XapianQueryParser();
+        $flags=
+            XapianQueryParser::FLAG_BOOLEAN |
+            XapianQueryParser::FLAG_PHRASE |
+            XapianQueryParser::FLAG_LOVEHATE |
+            XapianQueryParser::FLAG_WILDCARD |
+            XapianQueryParser::FLAG_PURE_NOT | XapianQueryParser::FLAG_BOOLEAN_ANY_CASE;
+
+$q = $qp->parse_query($equation, $flags);
+$h = $q->get_description();
+$h=preg_replace('~:\(pos=\d+?\)~', '', $h);
+
+echo "<p>Equation analysée par le query parser de xapian :<br /><code>$h</code></p>";
 
 die();
 
