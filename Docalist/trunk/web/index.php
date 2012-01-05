04c8db99dc3a0894f17b5ecf0efff5e2503cc7ad
Daniel Ménard <daniel-menard@users.noreply.github.com>
2012-01-05 18:40:33 +0000

    Docalist : ajout des front controllers index.php et debug.php.

---
+++ Docalist/trunk/web/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+$start=microtime(true);
+//define('SHORTINIT',true);
+require_once('wordpress/wp-load.php');
+$end = microtime(true);
+echo "Chargement wordpress : ", ($end-$start);
+// echo 'user : ', var_export(get_current_user());
+// var_export(current_user_can('manage_options'));
+// var_export(get_categories());
+
+ */
+require_once('../vendor/Fab2/Runtime.php');
+Runtime::setup();
