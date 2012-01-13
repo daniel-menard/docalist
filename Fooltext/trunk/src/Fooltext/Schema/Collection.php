97eef6db87d93c4f8f4e71a9f60abae187e5bbc5
Daniel Ménard <daniel-menard@users.noreply.github.com>
2012-01-13 08:44:25 +0000

    Nouvelle version de la classe Collection.

--- Fooltext/trunk/src/Fooltext/Schema/Collection.php
+++ Fooltext/trunk/src/Fooltext/Schema/Collection.php
@@ -15,118 +15,160 @@ namespace Fooltext\Schema;
 /**
  * Collection de documents.
  */
-class Collection extends Nodes
+class Collection extends Node
 {
-    protected static $defaultProperties = array
+    protected static $defaults = array
     (
-        // Nom de la collection
-        'name' => '',
-
         // Identifiant (préfixe) de la collection (code unique : a, b, aa, aaa)
 		'_id' => null,
 
-        // Dernier id utilisé pour numéroter les champs (entier positif non nul)
-    	'_lastid' => 0,
+        // Nom de la collection
+        'name' => '',
 
-    	// Nom de la classe utilisée pour représenter les documents
+        // Un libellé court décrivant la collection
+    	'label' => '',
+
+        // Description, notes, historique des modifs...
+        'description' => '',
+
+        // Nom de la classe utilisée pour représenter les documents
         // présents dans cette collection. Doit hériter de Fooltext\Document\Document
-        'documentClass' => '\\Fooltext\\Document\\Document',
+        'document' => '\\Fooltext\\Document\\Document',
+
+        // Nom du champ utilisé comme numéro unique des documents
+        'docid' => '',
     );
 
+    protected static $nodes = array
+    (
+    	'fields' => 'Fooltext\\Schema\\Fields',
+    	'aliases' => 'Fooltext\\Schema\\Aliases',
+    );
+
+//     public function __construct(array $data = array())
+//     {
+//         parent::__construct($data);
+
+//         if (isset($data['fields']))
+//         {
+//             $fields = $data['fields'];
+//             unset($data['fields']);
+//             if (! $fields instanceof Fields) $fields = new Fields($fields);
+//         }
+//         else
+//         {
+//             $fields = new Fields();
+//         }
+
+//         if (isset($data['aliases']))
+//         {
+//             $aliases = $data['aliases'];
+//             unset($data['aliases']);
+//             if (! $aliases instanceof Aliases) $aliases = new Aliases($aliases);
+//         }
+//         else
+//         {
+//             $aliases = new Aliases();
+//         }
+
+//         $this->data['fields'] = $fields;
+//         $this->data['aliases'] = $aliases;
+//     }
 
     /**
-     * Tableau permettant d'obtenir le nom d'un champ à partir de son id
+     * Ajoute un champ dans la collection.
      *
-     * @var array FieldName => FieldId
+     * @param Field $field le champ à ajouter.
+     *
+     * @return \Fooltext\Schema\Collection $this
+     *
+     * @throws \Exception si le champ a déjà un id.
      */
-    protected $id = array();
+//     public function addField($field)
+//     {
+//         // Vérifie que $field est un champ
+//         if(! $field instanceof Field) $field = new Field($field);
+
+//         // Attribue un id au champ
+//         if (! is_null($field->_id)) throw new \Exception('Le champ a déjà un _id');
+//         $field->_id = ++$this->_lastid;
+
+//         // Stocke le champ
+//         $this->data['fields']->add($field);
+//         $this->id[$field->_id] = $field->name;
+
+//         return $this;
+//     }
 
     /**
-    * Liste des collections présentes dans un schéma.
-    *
-    * @var array
-    */
-//     protected static $defaultChildren = array('fields','aliases');
-
-    protected static $labels = array
-    (
-        'main' => 'Liste des champs',
-    );
-
-    protected static $icons = array
-    (
-        'image' => 'zone--arrow.png',
-    );
-
+     * Indique si la collection contient des champs.
+     *
+     * @return boolean
+     */
+//     public function hasFields()
+//     {
+//         return ! empty($this->data['fields']);
+//     }
 
     /**
-     * @var array
+     * Indique si la collection contient un champ ayant le nom ou l'id indiqué.
+     *
+     * @param string $name
+     * @return boolean
      */
-    protected $fields = array();
-    protected $aliases = array();
+//     public function hasField($name)
+//     {
+//         return (isset($this->data['fields'][$name])) || (isset($this->id[$name]));
+//     }
 
-    public function __construct(array $properties = array())
-    {
-        if (isset($properties['fields']))
-        {
-            foreach($properties['fields'] as $field) $this->addField($field);
-            unset($properties['fields']);
-        }
+    /**
+     * Retourne le champ de la collection ayant le nom ou l'id indiqué.
+     *
+     * Génère une
+     * @param string|int $name
+     * @throws \Exception
+     */
+//     public function getField($name)
+//     {
+//         if (isset($this->data['fields'][$name])) return $this->data['fields'][$name];
+//         if (isset($this->id[$name])) return $this->fields[$this->id[$name]];
+//         throw new \NotFound("Le champ $name n'existe pas.");
+//     }
 
-        if (isset($properties['aliases']))
-        {
-            foreach($properties['aliases'] as $alias) $this->addAlias($alias);
-            unset($properties['aliases']);
-        }
+    /**
+     * Retourne la liste des champs de la collection.
+     *
+     * @return Collections
+     */
+//     public function getFields()
+//     {
+//         return $this->data['fields'];
+//     }
 
-        parent::__construct($properties);
-        $this->version = 2;
-    }
+    /**
+     * Supprime le champ ayant le nom ou l'id indiqué.
+     *
+     * Génère une exception si le champ indiqué n'existe pas.
+     *
+     * @param string $name
+     * @throws NotFound
+     * @return \Fooltext\Schema\Collection $this
+     */
+//     public function deleteField($name)
+//     {
+//         if (isset($this->data['fields'][$name]))
+//         {
+//             unset($this->id[$this->data['fields'][$name]->_id]);
+//             unset($this->data['fields'][$name]);
+//             return $this;
+//         }
 
-    public function addField($field)
-    {
-        // Vérifie que $field est un champ
-        if(! $field instanceof Field) $field = new Field($field);
+//         throw new \NotFound("Le champ $name n'existe pas.");
+//     }
 
-        // Attribue un id au champ
-        if (! is_null($field->_id)) throw new \Exception('Le champ a déjà un _id');
-        $field->_id = ++$this->_lastid;
-
-        // Stocke le champ
-        $this->addChild($this->fields, $field);
-        $this->id[$field->_id] = $field->name;
-
-        return $this;
-    }
-
-    // retourne un champ par nom ou par id
-    public function getField($name)
-    {
-        if (isset($this->fields[$name])) return $this->fields[$name];
-        if (isset($this->id[$name])) return $this->fields[$this->id[$name]];
-        throw new \Exception("Champ inexistant $name");
-    }
-
-    public function getFields()
-    {
-        return $this->fields;
-    }
-
-    public function deleteField($name)
-    {
-        if (isset($this->fields[$name]))
-        {
-            unset($this->id[$this->fields[$name]->_id]);
-            unset($this->fields[$name]);
-            return $this;
-        }
-
-        throw new \Exception("Champ inexistant $name");
-    }
-
-    public function getFieldName($id)
-    {
-        if (isset($this->id[$id])) return $this->id[$id];
-        return null;
-    }
+//     public function getFieldName($id)
+//     {
+//         if (isset($this->id[$id])) return $this->id[$id];
+//         return null;
+//     }
 }
