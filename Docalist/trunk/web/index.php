15bdbbacd66c9222e7d16ff6cbb745494e336172
Daniel Ménard <daniel-menard@users.noreply.github.com>
2012-01-17 16:47:03 +0000

    /web/index.php, debug.php : inclut le fichier autoload.php pour que les classes /vendor soient chargées automatiquement. Suppression du code de test qui figurait en commentaire.

--- Docalist/trunk/web/index.php
+++ Docalist/trunk/web/index.php
@@ -1,14 +1,4 @@
 <?php
-/*
-$start=microtime(true);
-//define('SHORTINIT',true);
-require_once('wordpress/wp-load.php');
-$end = microtime(true);
-echo "Chargement wordpress : ", ($end-$start);
-// echo 'user : ', var_export(get_current_user());
-// var_export(current_user_can('manage_options'));
-// var_export(get_categories());
-
- */
+require_once __DIR__ . '/../autoload.php';
 require_once('../vendor/Fab2/Runtime.php');
 Runtime::setup();
