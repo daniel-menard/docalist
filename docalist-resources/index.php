758ce0f45e330e15699fdafecb6aa638c3900884
Daniel Ménard <daniel-menard@users.noreply.github.com>
2012-11-06 10:56:24 +0000

    Fichier index.php de base avec le header wordpress nécessaire.

---
+++ docalist-resources/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Docalist-resources
+Plugin URI: http://docalist.org
+Description: Plugin docalist bidon pour tester les externals.
+Version: 1.0
+Author: Docalist
+Author URI: http://docalist.org
+*/
