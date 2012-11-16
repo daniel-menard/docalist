bb261211656ae61e08ff1ef54ecfe49a8a3e4eca
daniel.menard.35@gmail.com <daniel.menard.35@gmail.com@444a106b-eed5-e7b0-2fd9-c3bf2e9ceeb0>
2012-11-16 18:40:12 +0000

    Définition des CPT et des taxonomies gérées par docalist-resources.

---
+++ docalist-resources/trunk/parts/post-types/resource.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file is part of a "Docalist Resources" plugin.
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Resources
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
+ */
+
+namespace Docalist\Resources;
+
+return array(
+    'name' => 'resource',
+    'labels' => array(
+        'name' => __('Resources', 'docapress'), //addoption
+        'singular_name' => __('New resource', 'docapress'), // dans le menu
+        // nouveau de la admin bar
+        'add_new' => __('New resource', 'docapress'),
+        'all_items' => __('All resources', 'docapress'),
+        'add_new_item' => __('New resource', 'docapress'),
+        'edit_item' => __('Edit resource', 'docapress'),
+        'new_item' => __('Add new resource', 'docapress'),
+        'view_item' => __('View resource', 'docapress'), // dans admin bar
+        'search_items' => __('Search resource', 'docapress'),
+        'not_found' => __('No resources found.', 'docapress'),
+        'not_found_in_trash' => __('No resources found in trash.', 'docapress'),
+    ),
+    'public' => true,
+    'rewrite' => array(
+        'slug' => 'ressources', // addoption
+        'with_front' => false,
+    ),
+    'capability_type' => 'post',
+    'supports' => array(
+        'title',
+        'editor',
+        'thumbnail'
+    ),
+    //    'taxonomies' => array('post_tag'),
+    'has_archive' => true,
+    'edit_columns' => array(
+        'title' => __('Order Number'),
+        'author' => __('customer'),
+        'type' => 'Type',
+    )
+);
---
+++ docalist-resources/trunk/parts/taxonomies/resource-domain.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of a "Docalist Resources" plugin.
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Resources
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
+ */
+
+namespace Docalist\Resources;
+
+return array(
+    'name' => 'resource_domain',
+    'post_type' => array('resource'),
+    'configuration' => array(
+        'label' => __('Resource domains', 'docapress'),
+        'hierarchical' => true,
+        'show_ui' => true,
+        'query_var' => 'domaine', // addoption
+        'rewrite' => true,
+        /*
+         array
+         (
+         'slug' => 'ressources/domain',
+         'with_front' => false,
+         'hierarchical' => true,
+         ),
+         */
+    )
+);
