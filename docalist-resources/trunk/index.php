bd840da4cfce319729f3db8fe88aba990e2c0760
daniel.menard.35@gmail.com <daniel.menard.35@gmail.com@444a106b-eed5-e7b0-2fd9-c3bf2e9ceeb0>
2012-11-16 18:41:14 +0000

    Suppression du fichier index.php créé initialement (remplacé par main.php).

--- docalist-resources/trunk/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Plugin Name: Docalist-resources
-Plugin URI: http://docalist.org
-Description: Docalist : gestion d'un annuaire de ressources.
-Version: 1.0
-Author: Docalist
-Author URI: http://docalist.org
-*/
