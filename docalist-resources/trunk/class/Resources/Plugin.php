ac76dcc65046eac438a273f289e42ce1c82187c5
Daniel Ménard <daniel-menard@users.noreply.github.com>
2012-11-16 18:39:03 +0000

    Classe de base du plugin docalist-resources.

---
+++ docalist-resources/trunk/class/Resources/Plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of a "Docalist Resources" plugin.
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * @package     Docalist
+ * @subpackage  Resources
+ * @author      Daniel Ménard <daniel.menard@laposte.net>
+ * @version     SVN: $Id$
+ */
+
+namespace Docalist\Resources;
+use Docalist\Core\AbstractPlugin;
+
+/**
+ * Plugin de gestion d'un annuaire de ressources.
+ */
+class Plugin extends AbstractPlugin {
+}
