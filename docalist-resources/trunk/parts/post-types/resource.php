71bfdac95c21617767ee87704b6a65f0f5aea4cd
Daniel Ménard <daniel-menard@users.noreply.github.com>
2012-11-21 18:39:01 +0000

    docalist-resources : adaptation à la nouvelle version de docalist-core et ajout d'options permettant de choisir les libellés utilisés dans l'interface.

--- docalist-resources/trunk/parts/post-types/resource.php
+++ docalist-resources/trunk/parts/post-types/resource.php
@@ -16,18 +16,19 @@ namespace Docalist\Resources;
 return array(
     'name' => 'resource',
     'labels' => array(
-        'name' => __('Resources', 'docapress'), //addoption
-        'singular_name' => __('New resource', 'docapress'), // dans le menu
-        // nouveau de la admin bar
-        'add_new' => __('New resource', 'docapress'),
-        'all_items' => __('All resources', 'docapress'),
-        'add_new_item' => __('New resource', 'docapress'),
-        'edit_item' => __('Edit resource', 'docapress'),
-        'new_item' => __('Add new resource', 'docapress'),
-        'view_item' => __('View resource', 'docapress'), // dans admin bar
-        'search_items' => __('Search resource', 'docapress'),
-        'not_found' => __('No resources found.', 'docapress'),
-        'not_found_in_trash' => __('No resources found in trash.', 'docapress'),
+        'name' => $this->option('Resources.menu'),
+        'singular_name' => $this->option('Resources.name'),
+        'add_new' => $this->option('Resources.new'),
+        'add_new_item' => $this->option('Resources.new'),
+        'edit_item' => $this->option('Resources.edit'),
+        'new_item' => $this->option('Resources.new'),
+        'view_item' => $this->option('Resources.view'),
+        'search_items' => $this->option('Resources.search'),
+        'not_found' => $this->option('Resources.notfound'),
+        'not_found_in_trash' => $this->option('Resources.notfound'),
+        'all_items' => $this->option('Resources.all'),
+        'menu_name' => $this->option('Resources.menu'),
+        'name_admin_bar' => $this->option('Resources.name'),
     ),
     'public' => true,
     'rewrite' => array(
@@ -42,9 +43,11 @@ return array(
     ),
     //    'taxonomies' => array('post_tag'),
     'has_archive' => true,
+/*    
     'edit_columns' => array(
         'title' => __('Order Number'),
         'author' => __('customer'),
         'type' => 'Type',
     )
+*/ 
 );
